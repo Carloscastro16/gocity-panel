d0579e3a6727125824668a6f68e02a9ab73567c9
ccastro1016 <andycastro.001016@gmail.com>
2023-08-17 12:38:33 -0500

    Add Firebase and Angular Material

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'gocity-web',
+    appId: '1:612988024512:web:7c8785dee15718994e9f25',
+    storageBucket: 'gocity-web.appspot.com',
+    apiKey: 'AIzaSyCIsy-8aqbZDaLZhn4z2LyeNAJKC1xKaIg',
+    authDomain: 'gocity-web.firebaseapp.com',
+    messagingSenderId: '612988024512',
+    measurementId: 'G-W3T50Z1XTR',
+  },
   production: false
 };
 
